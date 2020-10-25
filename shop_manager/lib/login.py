8332a0e7090370db0de0496388773f7a6af4986c
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-26 02:32:36 +0600

    Updated login module

--- shop_manager/lib/login.py
+++ shop_manager/lib/login.py
@@ -2,7 +2,7 @@
 
 def login_validation(password):
     """ Function : For verifying user password """
-    with open("C:\\Users\\HP\\Desktop\\ShopApp\\shop_manager\\sources\\login.txt", "r+") as log:
+    with open("C:\\Users\\HP\\Desktop\\ShopApp\\shop_manager\\shop\\login.txt", "r+") as log:
         check_password = log.read()
         print(check_password)    
 
