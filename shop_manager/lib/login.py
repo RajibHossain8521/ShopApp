24ebdbb0bbce04d47a855c6dd342e74b0bb98159
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-26 02:06:05 +0600

    Create login file

---
+++ shop_manager/lib/login.py
@@ -0,0 +1,10 @@
+
+
+def login_validation(password):
+    """ Function : For verifying user password """
+    with open("C:\\Users\\HP\\Desktop\\ShopApp\\shop_manager\\sources\\login.txt", "r+") as log:
+        check_password = log.read()
+        print(check_password)    
+
+if __name__ == "__main__":
+    login(12345)
