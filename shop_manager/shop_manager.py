075624359d5a46906a4fa7a19228bcabdc3e7d0c
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-25 18:02:27 +0600

    file deleted

--- shop_manager/shop_manager.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-def main_window():
-    main_window = tk.Tk()
-    main_window.title("Shop Manager")
-    main_window.mainloop()
-
-if __name__ == "__main__":
-    main_window()
