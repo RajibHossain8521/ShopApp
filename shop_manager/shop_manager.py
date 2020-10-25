b6b1e5974033005a978635fd8375407728905746
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-25 13:35:11 +0600

    Create requirements.txt for project packages

---
+++ shop_manager/shop_manager.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+def main_window():
+    main_window = tk.Tk()
+    main_window.title("Shop Manager")
+    main_window.mainloop()
+
+if __name__ == "__main__":
+    main_window()
