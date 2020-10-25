9ffffadac98589913e4584d272cbb53770a937ef
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-25 20:58:54 +0600

    Main window updated

--- ShopApp/shop_manager/shop_manager.py
+++ ShopApp/shop_manager/shop_manager.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 def login(password):
     pass
@@ -15,27 +16,46 @@ def main_window():
 
     main_window = Tk()
     main_window.title("Shop Manager")
-    main_window.geometry("900x600")
+
+    # getting screen's height in pixels 
+    h = main_window.winfo_screenheight() 
+    # getting screen's width in pixels 
+    w = main_window.winfo_screenwidth() 
+
+    main_window.geometry("%dx%d" % (w,h))
+
+    # Set background Image
+    main_window_image = Image.open('C:\\Users\\HP\\Desktop\\ShopApp\\ShopApp\\sources\\images\\shopping2_photo.jpg')
+    main_window_image = main_window_image.resize((w//2, h), Image.ANTIALIAS)
+    main_window_image = ImageTk.PhotoImage(main_window_image)
+    l = Label(image=main_window_image, anchor=W)
+    l.pack(fill=BOTH, expand=True)
+
 
     # Greetings Text
     greetings = Label(main_window, text = "Welcome to Shop App!") 
     greetings.config(font =("Courier", 30))
-    greetings.place(x = 220, y = 80)
+    greetings.place(x = w//2+50, y = 80)
+
+    # Developer Info
+    developer_name = Label(main_window, text = "Developed By: Mohammad Rajib",)
+    developer_name.config(font =("Courier", 15))
+    developer_name.place(x = w//2+150, y=h-120)
 
     # Password field
     pass_name = Label(main_window, text = "Password")
     pass_name.config(font =("Courier", 15))
-    pass_name.place(x = 290,y = 210)
+    pass_name.place(x = w//2+100, y = 210)
     password_field = Entry(main_window, bd = 3, show="*")
-    password_field.place(x = 400,y = 210, height=30, width=150)
+    password_field.place(x = w//2+240, y = 210, height=30, width=150)
 
     # Entry Button
     password_entry = Button(main_window, text = "Entry")
-    password_entry.place(x = 430, y = 250, height=30, width=100)
+    password_entry.place(x = w//2+240, y = 250, height=30, width=150)
 
     # Set new password
-    new_password_button = Button(main_window, text="Set New Password", width=15, command=set_new_password_window)
-    new_password_button.place(x=422, y=300)
+    new_password_button = Button(main_window, text="Set New Password", command=set_new_password_window)
+    new_password_button.place(x = w//2+240, y=300, height=30, width=150)
 
     # Run tkinter window
     main_window.mainloop()
