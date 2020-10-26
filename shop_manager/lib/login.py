f2312f41fc706b0e0230e0942b76dcdc4a4f376f
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-27 00:39:00 +0600

    Encrypted password checking done

--- shop_manager/lib/login.py
+++ shop_manager/lib/login.py
@@ -1,13 +1,19 @@
+import hashlib
 
+file_path = 'C:/Users/HP/Desktop/ShopApp/shop_manager/shop/'
 
 def login_validation(password):
     """ Function : For verifying user password """
-    with open("C:\\Users\\HP\\Desktop\\ShopApp\\shop_manager\\shop\\login.txt", "r+") as log:
+    with open(file_path + "login.txt", "r+") as log:
         check_password = log.read()
-        if check_password == password:
+
+        password = str(password).encode()
+        encrypted_password = hashlib.sha224(password).hexdigest()
+
+        if check_password == encrypted_password:
             return True
         else:
             return False
 
-if __name__ == "__main__":
-    login_validation(12345)
+#if __name__ == "__main__":
+    #login_validation(1234)
