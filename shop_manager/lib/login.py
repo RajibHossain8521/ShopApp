19566e8611ea5d9ac8ef7ac9f9c36a0eb9c21a68
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-11-15 09:37:47 +0600

    Added exception handler

--- shop_manager/lib/login.py
+++ shop_manager/lib/login.py
@@ -4,16 +4,19 @@ file_path = 'C:/Users/HP/Desktop/ShopApp/shop_manager/shop/'
 
 def login_validation(password):
     """ Function : For verifying user password """
-    with open(file_path + "login.txt", "r+") as log:
-        check_password = log.read()
+    try:
+        with open(file_path + "login.txt", "r+") as log:
+            check_password = log.read()
 
-        password = str(password).encode()
-        encrypted_password = hashlib.sha224(password).hexdigest()
+            password = str(password).encode()
+            encrypted_password = hashlib.sha224(password).hexdigest()
 
-        if check_password == encrypted_password:
-            return True
-        else:
-            return False
+            if check_password == encrypted_password:
+                return True
+            else:
+                return False
+    except:
+        return False
 
 #if __name__ == "__main__":
     #login_validation(1234)
